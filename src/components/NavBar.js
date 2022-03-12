14bf3bad281c00640452f8abe963e21ea73bdc29
Augusto Ojeda <okarinex@gmail.com>
2022-03-12 12:10:37 -0300

    inicio del porfolio

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+    <>
+        <div>
+            <div>
+                <p>Español</p>
+            </div>
+            <div>
+                <p>Ingles</p>
+            </div>
+            <div>
+                <p>Tema</p>
+            </div>
+        </div>
+    </>
+    )
+}
+
+export default NavBar
