b6e4f7f2bc10f826fd8cb74f503e09e075931484
Augusto Ojeda <okarinex@gmail.com>
2022-08-15 17:19:05 -0300

    fixed estilos

--- src/components/Formacion.js
+++ src/components/Formacion.js
@@ -53,7 +53,7 @@ theme
 
 ?
 
-<Box bg='white' w='100%' h={{base: '450px', md: '600px'}} borderWidth='1px'>
+<Box display={'flex'} justifyContent={'center'} alignItems={'center'} flexDirection={'column'} bg='white' w='100%' h={{base: '550px', md: '700px'}} borderWidth='1px'>
 
       <Heading bgGradient='linear(to-l, red.500, #FF0080)' bgClip='text' fontSize={{base: '2xl', sm: '3xl', md: '5xl', lg: '6xl'}} fontWeight={'bold'} textAlign='center' p={2} mb={10}>
         {
@@ -71,12 +71,14 @@ theme
 
         <Box 
           display={'flex'} 
+          justifyContent={'center'}
           alignItems={'center'} 
-          h={{ base: '320px', md: 'auto'}} 
+          h={{ base: '320px', md: 'auto'}}
+          w={'90%'} 
           borderWidth='1px' borderColor={'red.500'} 
           borderRadius={'base'}  mb={5}
           mx={{base: '20px', sm: '20px', md: '20px', lg: '80px', xl: '120px'}} 
-          px={4}
+          p={10}
           >
               
               <Stack direction={'row'} spacing={5} overflowX={'auto'}>
@@ -87,9 +89,9 @@ theme
       
     :
     
-    <Box bgImage={galaxyAll4} bgRepeat={'no-repeat'} bgSize={'cover'} w='100%' h={{base: '460px', md: '600px'}}>
+    <Box bgImage={galaxyAll4} bgRepeat={'no-repeat'} bgSize={'cover'} w='100%' h={{base: '500px', md: '600px'}}>
 
-  <Box bgGradient={'linear(to-b, blackAlpha.900, blackAlpha.600)'}>
+  <Box  bgGradient={'linear(to-b, blackAlpha.900, blackAlpha.600)'}>
         <Heading  bgGradient='linear(to-l,  #7928CA, #FF0080)'  bgClip='text'  fontFamily={'sans-serif'} fontSize={{base: '2xl', sm: '3xl', md: '5xl', lg: '6xl'}} fontWeight={'bold'} textAlign='center' p={2} mb={10}>
           {
           traslate
@@ -104,18 +106,20 @@ theme
           }
           </Heading>
   </Box>
-      <Box 
-      display={'flex'}
-      alignItems={'center'}
-      mx={{base: '20px', sm: '30px', md: '40px', lg: '60px', xl: '100px'}}
-      >
-            
-              <Stack direction={'row'} spacing='25px' boxShadow='base' borderRadius={'md'} overflowX={'auto'} px={10} py={10}>
-                {formation.map(e=> ( <FormacionStyleDark key={e.name} name={e.name} img={e.img} href={e.href} state={traslate ? e.state : e.stateEn} />))}
-              </Stack>
+      <Box display={'flex'} justifyContent={'center'} alignItems={'center'} flexDirection={'column'} px={5} ml={{base: 0, xl: '120px'}}>
+          <Box 
+          display={'flex'}
+          alignItems={'center'}
+          w={'100%'}
+          mx={{base: '20px', sm: '30px', md: '40px', lg: '60px', xl: '100px'}}
+          >
+
+                  <Stack direction={'row'} spacing='25px' boxShadow='base' borderRadius={'md'} overflowX={'auto'} px={10} py={10}>
+                    {formation.map(e=> ( <FormacionStyleDark key={e.name} name={e.name} img={e.img} href={e.href} state={traslate ? e.state : e.stateEn} />))}
+                  </Stack>
+          </Box>
       </Box>
     </Box>
-    
     }
     </>
   )
